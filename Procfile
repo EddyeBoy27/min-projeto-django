43eaea6ab0d507d0c91bf2c060133c49c7877e6c
eddyeboy27 <lipe0503@gmail.com>
2021-01-20 21:59:12 -0300

    Atualização

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn --pythonpath estoque_project estoque_project.wsgi --log-file -
+release: python manage.py migrate
--- estoque_project/estoque_project/settings.py
+++ estoque_project/estoque_project/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '3w(k7o-9$!eutivzkau54)f5($c_k^sl&m%5687=6++k25kb1g'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '3w(k7o-9$!eutivzkau54)f5($c_k^sl&m%5687=6++k25kb1g'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
